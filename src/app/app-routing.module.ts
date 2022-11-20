03b1764c2f05ccbb3e4df0e830f4af9554a7c105
ahmedl1710 <75045795+ahmedl1710@users.noreply.github.com>
2022-11-20 14:25:07 +0100

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffresEmploisComponent } from './offres-emplois/offres-emplois.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+//Type ROutes c'est un type predefinie
+//ici dans le tableau routes on va ajouter nos path
+//eviter d'utiliser les "/" dans les pâth  
+//l'ordre des routes est important 
+//l'emplacement de NotFoundComponent doit etre a la fin de la table 
+const routes: Routes = [
+  {path:'products', component:ProductsComponent}, //inctanciation d'une route sous format Json
+  {path:'offre-emploi', component:OffresEmploisComponent},
+  {path:'',redirectTo:'products',pathMatch:'full'}, //path de redirection
+  {path:'**',component:NotFoundComponent}//la pth ** sgnifie ay chaine de caractere qui n'est pas dans la table des routes
+];
 
 @NgModule({
+  //
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { OffresEmploisComponent } from './offres-emplois/offres-emplois.component';
+import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    OffresEmploisComponent,
+    NotFoundComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+
+
+  //ici on declare les services
   providers: [],
   bootstrap: [AppComponent]
 })
